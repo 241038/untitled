29cca84a96b5f6e9af6d58bdc8fb6d6edb7ca8c5
241038 <241038@std.hi-joho.ac.jp>
2025-05-19 11:31:31 +0900

    かだいたち(^_-)-☆

---
+++ src/Fukusyu3.java
@@ -0,0 +1,11 @@
+import java.util.logging.XMLFormatter;
+
+public class Fukusyu3 {
+    public static void main(String[] args) {
+        string str1;
+        char chr1;
+        str1 = iphon;
+        chr1 = X;
+        System.out.println(str1 + chr1);
+    }
+}
