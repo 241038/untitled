75bfe091d3063c86df969ca02775ecd559b773cb
241038 <241038@std.hi-joho.ac.jp>
2025-05-19 11:18:07 +0900

    かだいたち(^_-)-☆

---
+++ src/Review1.java
@@ -0,0 +1,9 @@
+public class Review1 {
+    public static void main(String[] args) {
+        System.out.print("こんにちは。");
+        System.out.println("今日の天気は"+ \t +"晴れです。");
+        System.out.println("今日は一日" + "洗濯日和です。");
+        System.out.print("外に干しても大丈夫そうです。");
+        System.out.println("文字列"\""+"で囲みましょう。");
+    }
+}
